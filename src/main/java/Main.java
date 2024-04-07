655a0d511ffb65a6337092cbe811f045d3f89e46
Vitaliy Murzin <murzinvit@yandex.ru>
2024-04-07 19:01:37 +0500

    step 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
-import com.opencsv.exceptions.CsvValidationException;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -13,23 +13,26 @@ public class Main {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
+        list.forEach(System.out::println);
     }
 
-    protected static List<Employee> parseCSV(String[] columnMapping, String fileName){
-        List<Employee> employees = new ArrayList<Employee>();
-        ColumnPositionMappingStrategy columnPositionMappingStrategy = new ColumnPositionMappingStrategy();
-        columnPositionMappingStrategy.setType(Employee.class);
-        columnPositionMappingStrategy.setColumnMapping(columnMapping);
+    protected static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        List<Employee> staff = new ArrayList<Employee>();
 
-        try(CSVReader csvReader = new CSVReader(new FileReader(fileName))){
-           String[] csvRecord;
-           while((csvRecord = csvReader.readNext()) != null){
-               System.out.println(Arrays.toString(csvRecord));
-           }
-        }catch (IOException | CsvValidationException ex) {
+        ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+        strategy.setType(Employee.class);
+        strategy.setColumnMapping(columnMapping);
+
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            staff = csv.parse();
+
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
 
-        return employees;
+        return staff;
     }
 }
